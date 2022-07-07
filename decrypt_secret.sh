f488bdee3540fc9466a66e6a9ca8d2b36a596d6b
Mahesh Nagmal <mahesh@opsfuse.com>
2022-07-07 09:12:20 +0530

    updated

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -5,4 +5,5 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/el-dev-vm2.pem el-dev-vm2.pem.gpg
+--output $HOME/secrets/el-dev-vm2.key el-dev-vm2.pem.gpg
+openssl rsa -in $HOME/secrets/el-dev-vm2.key -text > el-dev-vm2.pem
